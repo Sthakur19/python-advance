386995405d9f87e0b43c5fc3ce68c0fcbcb189d9
Sthakur19 <STHAKUR@ibs-b.hu>
2023-06-08 16:12:52 +0530

    try except

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -1,11 +1,20 @@
 
 import pandas
+
 data = pandas.read_csv('nato_phonetic_alphabet.csv')
 
 new_dict = { row.letter: row.code for (index, row) in data.iterrows()}
 
-enter_word = input("Enter the word : ").upper()
 
-new_list = [ new_dict[word] for word in enter_word]
 
-print(new_list)
+def generate_phonetic():
+    enter_word = input("Enter the word : ").upper()
+    try:
+        new_list = [ new_dict[word] for word in enter_word]
+    except KeyError:
+        print('Its not alphabet')
+        generate_phonetic()
+    else:
+        print(new_list)
+    
+generate_phonetic()
